30596a311783b34cf0954c069990ed8ea25038df
JMDirksen <jeftadirksen@gmail.com>
2024-01-01 17:07:41 +0100

    Updated

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
 FROM ubuntu
-RUN apt-get update && apt-get install -y openjdk-17-jre-headless curl jq
-WORKDIR /root
-COPY ./run.sh ./
-ADD https://github.com/Tiiffi/mcrcon/releases/download/v0.7.2/mcrcon-0.7.2-linux-x86-64.tar.gz ./mcrcon.tar.gz
-RUN tar -zxf mcrcon.tar.gz && rm mcrcon.tar.gz
-VOLUME /minecraft
+RUN apt-get update && apt-get install -y openjdk-21-jre-headless curl jq
+EXPOSE 25565
+VOLUME /data/server
+WORKDIR /data
+COPY --chmod=700 run.sh .
 
 # Set the java -Xms and -Xmx parameters
 ENV MEMORY=4G
@@ -12,61 +11,64 @@ ENV MEMORY=4G
 # Set following to TRUE from run command indicating your agreement to the EULA (https://account.mojang.com/documents/minecraft_eula).
 ENV EULA=false
 
-# Interval in seconds for restarting the minecraft server
-ENV RESTART_INTERVAL=36000
-
 # server.properties settings
-ENV allow_flight=false
-ENV allow_nether=true
-ENV broadcast_console_to_ops=true
-ENV broadcast_rcon_to_ops=true
+ENV allow_flight=FALSE
+ENV allow_nether=TRUE
+ENV broadcast_console_to_ops=TRUE
+ENV broadcast_rcon_to_ops=TRUE
 ENV difficulty=easy
-ENV enable_command_block=false
-ENV enable_jmx_monitoring=false
-ENV enable_query=false
-ENV enable_rcon=true
-ENV enable_status=true
-ENV enforce_whitelist=false
+ENV enable_command_block=FALSE
+ENV enable_jmx_monitoring=FALSE
+ENV enable_query=FALSE
+ENV enable_rcon=TRUE
+ENV enable_status=TRUE
+ENV enforce_secure_profile=TRUE
+ENV enforce_whitelist=FALSE
 ENV entity_broadcast_range_percentage=100
-ENV force_gamemode=false
+ENV force_gamemode=FALSE
 ENV function_permission_level=2
 ENV gamemode=survival
-ENV generate_structures=true
+ENV generate_structures=TRUE
 ENV generator_settings={}
-ENV hardcore=false
-ENV hide_online_players=false
+ENV hardcore=FALSE
+ENV hide_online_players=FALSE
+ENV initial_disabled_packs=
+ENV initial_enabled_packs=vanilla
 ENV level_name=world
 ENV level_seed=
 ENV level_type=default
+ENV log_ips=TRUE
+ENV max_chained_neighbor_updates=1000000
 ENV max_players=20
 ENV max_tick_time=60000
 ENV max_world_size=29999984
 ENV motd="A Minecraft Server"
 ENV network_compression_threshold=256
-ENV online_mode=true
+ENV online_mode=TRUE
 ENV op_permission_level=4
 ENV player_idle_timeout=0
-ENV prevent_proxy_connections=false
-ENV pvp=true
+ENV prevent_proxy_connections=FALSE
+ENV pvp=TRUE
 ENV query_port=25565
 ENV rate_limit=0
 ENV rcon_password=password
 ENV rcon_port=25575
-ENV require_resource_pack=false
+ENV require_resource_pack=FALSE
 ENV resource_pack=
+ENV resource_pack_id=
 ENV resource_pack_prompt=
 ENV resource_pack_sha1=
 ENV server_ip=
 ENV server_port=25565
 ENV simulation_distance=10
-ENV spawn_animals=true
-ENV spawn_monsters=true
-ENV spawn_npcs=true
+ENV spawn_animals=TRUE
+ENV spawn_monsters=TRUE
+ENV spawn_npcs=TRUE
 ENV spawn_protection=16
-ENV sync_chunk_writes=true
+ENV sync_chunk_writes=FALSE
 ENV text_filtering_config=
-ENV use_native_transport=true
+ENV use_native_transport=TRUE
 ENV view_distance=10
-ENV white_list=false
+ENV white_list=FALSE
 
-CMD /bin/bash run.sh
+CMD ./run.sh
--- run.sh
+++ run.sh
@@ -1,85 +1,68 @@
 #!/bin/bash
 
-cd /minecraft
-
-echo "eula=$EULA" > eula.txt
-
-echo "allow-flight=${allow_flight}" > server.properties
-echo "allow-nether=${allow_nether}" >> server.properties
-echo "broadcast-console-to-ops=${broadcast_console_to_ops}" >> server.properties
-echo "broadcast-rcon-to-ops=${broadcast_rcon_to_ops}" >> server.properties
-echo "difficulty=${difficulty}" >> server.properties
-echo "enable-command-block=${enable_command_block}" >> server.properties
-echo "enable-jmx-monitoring=${enable_jmx_monitoring}" >> server.properties
-echo "enable-query=${enable_query}" >> server.properties
-echo "enable-rcon=${enable_rcon}" >> server.properties
-echo "enable-status=${enable_status}" >> server.properties
-echo "enforce-whitelist=${enforce_whitelist}" >> server.properties
-echo "entity-broadcast-range-percentage=${entity_broadcast_range_percentage}" >> server.properties
-echo "force-gamemode=${force_gamemode}" >> server.properties
-echo "function-permission-level=${function_permission-level}" >> server.properties
-echo "gamemode=${gamemode}" >> server.properties
-echo "generate-structures=${generate_structures}" >> server.properties
-echo "generator-settings=${generator_settings}" >> server.properties
-echo "hardcore=${hardcore}" >> server.properties
-echo "hide-online-players=${hide_online_players}" >> server.properties
-echo "level-name=${level_name}" >> server.properties
-echo "level-seed=${level_seed}" >> server.properties
-echo "level-type=${level_type}" >> server.properties
-echo "max-players=${max_players}" >> server.properties
-echo "max-tick-time=${max_tick_time}" >> server.properties
-echo "max-world-size=${max_world_size}" >> server.properties
-echo "motd=${motd}" >> server.properties
-echo "network-compression-threshold=${network_compression_threshold}" >> server.properties
-echo "online-mode=${online_mode}" >> server.properties
-echo "op-permission-level=${op_permission_level}" >> server.properties
-echo "player-idle-timeout=${player_idle_timeout}" >> server.properties
-echo "prevent-proxy-connections=${prevent_proxy_connections}" >> server.properties
-echo "pvp=${pvp}" >> server.properties
-echo "query.port=${query_port}" >> server.properties
-echo "rate-limit=${rate_limit}" >> server.properties
-echo "rcon.password=${rcon_password}" >> server.properties
-echo "rcon.port=${rcon_port}" >> server.properties
-echo "require-resource-pack=${require_resource_pack}" >> server.properties
-echo "resource-pack=${resource_pack}" >> server.properties
-echo "resource-pack-prompt=${resource_pack_prompt}" >> server.properties
-echo "resource-pack-sha1=${resource_pack_sha1}" >> server.properties
-echo "server-ip=${server_ip}" >> server.properties
-echo "server-port=${server_port}" >> server.properties
-echo "simulation-distance=${simulation_distance}" >> server.properties
-echo "spawn-animals=${spawn_animals}" >> server.properties
-echo "spawn-monsters=${spawn_monsters}" >> server.properties
-echo "spawn-npcs=${spawn_npcs}" >> server.properties
-echo "spawn-protection=${spawn_protection}" >> server.properties
-echo "sync-chunk-writes=${sync_chunk-writes}" >> server.properties
-echo "text-filtering-config=${text_filtering_config}" >> server.properties
-echo "use-native-transport=${use_native_transport}" >> server.properties
-echo "view-distance=${view_distance}" >> server.properties
-echo "white-list=${white_list}" >> server.properties
-
-{
-  while true
-  do
-    echo "Starting restart timer (${RESTART_INTERVAL} seconds)"
-    sleep ${RESTART_INTERVAL}
-    echo "Initiating restart..."
-    /root/mcrcon -P ${rcon_port} -p ${rcon_password} -w 10 \
-      list \
-      "tellraw @a {\"text\":\"Server will restart in 1 minute...\",\"color\":\"red\"}" \
-      list \
-      list \
-      "tellraw @a {\"text\":\"Server will restart in 30 seconds...\",\"color\":\"red\"}" \
-      list \
-      "tellraw @a {\"text\":\"Server will restart in 10 seconds...\",\"color\":\"red\"}" \
-      stop
-  done
-} &
-
-while true
-do
+main () {
+  echo "eula=$EULA" > server/eula.txt
+  echo "allow-flight=${allow_flight}" > server/server.properties
+  echo "allow-nether=${allow_nether}" >> server/server.properties
+  echo "broadcast-console-to-ops=${broadcast_console_to_ops}" >> server/server.properties
+  echo "broadcast-rcon-to-ops=${broadcast_rcon_to_ops}" >> server/server.properties
+  echo "difficulty=${difficulty}" >> server/server.properties
+  echo "enable-command-block=${enable_command_block}" >> server/server.properties
+  echo "enable-jmx-monitoring=${enable_jmx_monitoring}" >> server/server.properties
+  echo "enable-query=${enable_query}" >> server/server.properties
+  echo "enable-rcon=${enable_rcon}" >> server/server.properties
+  echo "enable-status=${enable_status}" >> server/server.properties
+  echo "enforce-secure-profile=${enforce_secure_profile}" >> server/server.properties
+  echo "enforce-whitelist=${enforce_whitelist}" >> server/server.properties
+  echo "entity-broadcast-range-percentage=${entity_broadcast_range_percentage}" >> server/server.properties
+  echo "force-gamemode=${force_gamemode}" >> server/server.properties
+  echo "function-permission-level=${function_permission_level}" >> server/server.properties
+  echo "gamemode=${gamemode}" >> server/server.properties
+  echo "generate-structures=${generate_structures}" >> server/server.properties
+  echo "generator-settings=${generator_settings}" >> server/server.properties
+  echo "hardcore=${hardcore}" >> server/server.properties
+  echo "hide-online-players=${hide_online_players}" >> server/server.properties
+  echo "initial-disabled-packs=${initial_disabled_packs}" >> server/server.properties
+  echo "initial-enabled-packs=${initial_enabled_packs}" >> server/server.properties
+  echo "level-name=${level_name}" >> server/server.properties
+  echo "level-seed=${level_seed}" >> server/server.properties
+  echo "level-type=${level_type}" >> server/server.properties
+  echo "log-ips=${log_ips}" >> server/server.properties
+  echo "max-chained-neighbor-updates=${max_chained_neighbor_updates}" >> server/server.properties
+  echo "max-players=${max_players}" >> server/server.properties
+  echo "max-tick-time=${max_tick_time}" >> server/server.properties
+  echo "max-world-size=${max_world_size}" >> server/server.properties
+  echo "motd=${motd}" >> server/server.properties
+  echo "network-compression-threshold=${network_compression_threshold}" >> server/server.properties
+  echo "online-mode=${online_mode}" >> server/server.properties
+  echo "op-permission-level=${op_permission_level}" >> server/server.properties
+  echo "player-idle-timeout=${player_idle_timeout}" >> server/server.properties
+  echo "prevent-proxy-connections=${prevent_proxy_connections}" >> server/server.properties
+  echo "pvp=${pvp}" >> server/server.properties
+  echo "query.port=${query_port}" >> server/server.properties
+  echo "rate-limit=${rate_limit}" >> server/server.properties
+  echo "rcon.password=${rcon_password}" >> server/server.properties
+  echo "rcon.port=${rcon_port}" >> server/server.properties
+  echo "require-resource-pack=${require_resource_pack}" >> server/server.properties
+  echo "resource-pack=${resource_pack}" >> server/server.properties
+  echo "resource-pack-id=${resource_pack_id}" >> server/server.properties
+  echo "resource-pack-prompt=${resource_pack_prompt}" >> server/server.properties
+  echo "resource-pack-sha1=${resource_pack_sha1}" >> server/server.properties
+  echo "server-ip=${server_ip}" >> server/server.properties
+  echo "server-port=${server_port}" >> server/server.properties
+  echo "simulation-distance=${simulation_distance}" >> server/server.properties
+  echo "spawn-animals=${spawn_animals}" >> server/server.properties
+  echo "spawn-monsters=${spawn_monsters}" >> server/server.properties
+  echo "spawn-npcs=${spawn_npcs}" >> server/server.properties
+  echo "spawn-protection=${spawn_protection}" >> server/server.properties
+  echo "sync-chunk-writes=${sync_chunk_writes}" >> server/server.properties
+  echo "text-filtering-config=${text_filtering_config}" >> server/server.properties
+  echo "use-native-transport=${use_native_transport}" >> server/server.properties
+  echo "view-distance=${view_distance}" >> server/server.properties
+  echo "white-list=${white_list}" >> server/server.properties
 
   # Get current version
-  [ -f "version.txt" ] && current_version=$(cat "version.txt") || current_version=0
+  [ -f "version" ] && current_version=$(cat "version") || current_version=0
   echo "Current version: $current_version"
 
   # Get latest version
@@ -92,16 +75,16 @@ do
     echo "Downloading new version..."
     version_json_url=$(curl -s "$manifest_url" | jq -r ".versions[] | select(.id==\"$latest_version\") | .url")
     server_url=$(curl -s "$version_json_url" | jq -r ".downloads.server.url")
-    curl -s -o server.jar "$server_url"
-    echo "$latest_version" > "version.txt"
+    curl -s -o "server/server.jar" "$server_url"
+    echo "$latest_version" > "version"
     echo "Updated!"
   }
 
   # Starting server
   echo "Set memory: $MEMORY"
   echo "Starting server..."
+  cd server
   java -Xms$MEMORY -Xmx$MEMORY -jar server.jar
-  echo "Restarting in 3 seconds..."
-  sleep 3
+}
 
-done
+main
