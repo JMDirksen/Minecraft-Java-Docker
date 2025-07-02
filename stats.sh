c8cc30c512cfbd87be6643f43c747bd97e260649
JMDirksen <jeftadirksen@gmail.com>
2025-07-02 21:00:50 +0200

    tz not working

--- stats.sh
+++ stats.sh
@@ -5,7 +5,7 @@ sleep 60
 while [ -n "$DEPLOYMENTID" ]; do
   limit=$((60 / 5 * 24 * 30))
   online=$(find server/world/stats -mmin -5 -type f | wc -l)
-  dt=$(TZ=Europe/Amsterdam date +"%Y-%m-%d+%H:%M")
+  dt=$(date +"%Y-%m-%d+%H:%M")
   uri="https://script.google.com/macros/s/$DEPLOYMENTID/exec?stats=$dt;$online&limit=$limit"
   echo "Players online: $online"
   curl --no-progress-meter "$uri" -o /dev/null
